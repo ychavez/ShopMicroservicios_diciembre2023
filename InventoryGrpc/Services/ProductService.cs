1b3afda75912478ed48d352d3c3e7649c1a6f4a9
YAEL <you400@gmail.com>
2023-11-29 19:59:46 -0600

    grpc1

---
+++ InventoryGrpc/Services/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using InventoryGrpc.Protos;
+
+namespace InventoryGrpc.Services
+{
+    public class ProductService : Existence.ExistenceBase
+    {
+        public override Task<ProductExistenceReply> checkExistence(ProductRequest request, ServerCallContext context)
+        {
+            //logica de negocios va aqui
+
+            return Task.FromResult(new ProductExistenceReply() { ProductQTY = 12 });
+        }
+
+    }
+}
