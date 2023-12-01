ccbdef7c55b33320d90a4ba1ea68344ffb855bc1
YAEL <you400@gmail.com>
2023-11-30 20:01:53 -0600

    multitenant

---
+++ Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase, IMultiTenant
+    {
+        public string UserName { get; set; } = null!;
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+
+        public int PaymentMethod { get; set; }
+        public Guid TenantId { get; set; }
+    }
+}
---
+++ Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public Guid TenantId { get; private set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "user";
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = "user";
+                        break;
+
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var entityType = entity.ClrType;
+
+                if (!typeof(IMultiTenant).IsAssignableFrom(entityType)) continue;
+
+                var method = typeof(OrderContext).GetMethod(nameof(MultitenantExpression),
+                    BindingFlags.NonPublic| BindingFlags.Static)?.MakeGenericMethod(entityType);
+
+                var filter = method?.Invoke(null,new object[] { this });
+
+                entity.SetQueryFilter((LambdaExpression)filter!);
+
+                entity.AddIndex(entity.FindProperty(nameof(IMultiTenant.TenantId))!);
+
+            }
+        }
+
+        private static LambdaExpression MultitenantExpression<T>(OrderContext context)
+            where T : EntityBase, IMultiTenant
+        {
+            Expression<Func<T, bool>> tenantFilter = x => x.TenantId == context.TenantId;
+            return tenantFilter;
+        }
+    }
+}
