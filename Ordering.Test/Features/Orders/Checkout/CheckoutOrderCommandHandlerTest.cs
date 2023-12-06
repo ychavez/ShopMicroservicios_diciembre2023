8aa548a5260419821f107b2922a950250ab8e9b7
YAEL <you400@gmail.com>
2023-12-05 19:03:37 -0600

    unit tests

--- Ordering.Test/Features/Orders/Checkout/CheckoutOrderCommandHandlerTest.cs
+++ Ordering.Test/Features/Orders/Checkout/CheckoutOrderCommandHandlerTest.cs
@@ -24,7 +24,7 @@ namespace Ordering.Test.Features.Orders.Checkout
 
 
         [Fact]
-        public async Task Handle_ShouldAddNewOrderToRepository() 
+        public async Task Handle_ShouldAddNewOrderToRepository()
         {
             //Arrange
             var command = new CheckoutOrderCommand()
@@ -38,17 +38,57 @@ namespace Ordering.Test.Features.Orders.Checkout
             };
 
             var orderEntity = new Order { Id = 12 };
-            _mapperMock.Setup(x=> x.Map<Order>(command)).Returns(orderEntity);
+            _mapperMock.Setup(x => x.Map<Order>(command)).Returns(orderEntity);
             _repositoryMock.Setup(x => x.AddAsync(orderEntity)).ReturnsAsync(orderEntity);
 
             //Act
             var result = await _hanlder.Handle(command, CancellationToken.None);
 
             //Assert
-            _repositoryMock.Verify(r=> r.AddAsync(orderEntity),Times.Once);
-            _mapperMock.Verify(m=> m.Map<Order>(command),  Times.Once);
+            _repositoryMock.Verify(r => r.AddAsync(orderEntity), Times.Once);
+            _mapperMock.Verify(m => m.Map<Order>(command), Times.Once);
             Assert.Equal(result, orderEntity.Id);
-        
+
+        }
+
+
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 3, 4)]
+        [InlineData(1, 4, 5)]
+        [InlineData(1, 5, 6)]
+        public async Task OnePlusOne(int value1, int value2, int expected) 
+        {
+            //Arrange 
+            int a = value1;
+            int b = value2;
+
+            //Act
+            var result = new Calculator().Sum(a, b);
+
+
+            //Assert
+            Assert.Equal(result, expected);
+        }
+
+
+
+
+
+    }
+
+    public class Calculator()
+    {
+        public int Sum(int a, int b)
+        {
+            if (b == 5)
+            {
+                b++;
+            }
+            return a + b;
+
         }
     }
 }
