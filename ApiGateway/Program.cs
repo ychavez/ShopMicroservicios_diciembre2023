fd4c28e11b68cf8836f8cdb3443c2b34c02942d1
YAEL <you400@gmail.com>
2023-12-08 19:06:32 -0600

    apigateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,44 @@
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.ConfigureAppConfiguration((ctx, conf) =>
+            {
+                conf.AddJsonFile(Path.Combine("Configuration", "configuration.json"));
+            });
+
+            builder.Services.AddOcelot();
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            await app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
